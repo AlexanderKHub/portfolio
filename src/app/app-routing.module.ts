a68acf4e49f131ad538202ba7cabb9a9666f99a3
Alexander K <alexander.k.2000@web.de>
2022-05-02 15:57:46 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: IntroductionComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
